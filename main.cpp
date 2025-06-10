a7ca9fa232109c248e0736180de6e97325a51d59
liorpernik <liorpernik@mail.tau.ac.il>
2025-06-10 16:24:49 +0300

    main

--- main.cpp
+++ main.cpp
@@ -15,26 +15,27 @@ int main(int argc, char** argv)
         if(argc > 1) {
             filePath = argv[1];
         }
-//        auto playerFactory = std::make_unique<MyPlayerFactory>();
-//        auto tankFactory = std::make_unique<MyTankAlgorithmFactory>();
-//        GameManager game(std::move(playerFactory), std::move(tankFactory));
-//        game.readBoard(filePath);
-//        game.run();
-//
-
-    auto playerFactory = std::make_unique<MyPlayerFactory>();
-    auto tankFactory = std::make_unique<MyTankAlgorithmFactory>();
-    for(int i=6;i<7;i++){
-      std::cout <<"test "<<i <<"\n";
+        //        auto playerFactory = std::make_unique<MyPlayerFactory>();
+        //        auto tankFactory = std::make_unique<MyTankAlgorithmFactory>();
+        //        GameManager game(std::move(playerFactory), std::move(tankFactory));
+        //        game.readBoard(filePath);
+        //        game.run();
+        //
 
         auto playerFactory = std::make_unique<MyPlayerFactory>();
         auto tankFactory = std::make_unique<MyTankAlgorithmFactory>();
-        filePath = "./inputs/t" + to_string(i) + ".txt";
-        GameManager game(std::move(playerFactory), std::move(tankFactory));
-        game.readBoard(filePath);
-        game.run();
-        std::cout <<"done\n";
-      }
+        for(int i=6;i<7;++i){
+            std::cout <<"test "<<i <<"\n";
+
+            auto playerFactory = std::make_unique<MyPlayerFactory>();
+            auto tankFactory = std::make_unique<MyTankAlgorithmFactory>();
+            filePath = "C:\\Users\\liorp\\Documents\\Uni_HW\\Cpp\\HW2\\Tank-2\\inputs\\t" + to_string(i) + ".txt";
+            GameManager game(std::move(playerFactory), std::move(tankFactory));
+            game.readBoard(filePath);
+            game.run();
+            std::cout <<"done\n";
+
+        }
     } catch (const std::exception& e) {
         std::clog << e.what() << std::endl;
         return 1;
